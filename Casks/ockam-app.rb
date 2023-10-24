dee8a687d0c27c92038523add2757956857f2f51
Michael Uti <utimichael9@gmail.com>
2023-10-24 13:12:04 +0100

    Update

--- Casks/ockam-app.rb
+++ Casks/ockam-app.rb
@@ -2,15 +2,15 @@ cask "ockam-app" do
   version "ockam_v0.99.0"
   name "Ockam"
   desc "End-to-end encryption and mutual authentication for distributed applications"
-  homepage "https://github.com/build-trust/ockam"
+  homepage "https://github.com/metaclips/ockam"
 
   if Hardware::CPU.arm?
-    url "https://github.com/build-trust/ockam/releases/download/ockam_v0.99.0/ockam.app.aarch64-apple-darwin.dmg"
+    url "https://github.com/metaclips/ockam/releases/download/ockam_v0.99.0/ockam.app.aarch64-apple-darwin.dmg"
     sha256 "ab1b4bf3c21120e16d676acbc77de405a2b5c53f1f34ef83153397ea3b1edce5"
    end
 
   if Hardware::CPU.intel?
-    url "https://github.com/build-trust/ockam/releases/download/ockam_v0.99.0/ockam.app.x86_64-apple-darwin.dmg"
+    url "https://github.com/metaclips/ockam/releases/download/ockam_v0.99.0/ockam.app.x86_64-apple-darwin.dmg"
     sha256 "3ea7b8c560ba231e2894cf2c449239069058fd1137bf9d3db503dae36b157fd3"
   end
 
--- ockam.rb
+++ ockam.rb
@@ -3,12 +3,12 @@
 
 class Ockam < Formula
   desc "End-to-end encryption and mutual authentication for distributed applications"
-  homepage "https://github.com/build-trust/ockam"
+  homepage "https://github.com/metaclips/ockam"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.99.0/ockam.aarch64-apple-darwin"
+      url "https://github.com/metaclips/ockam/releases/download/ockam_v0.99.0/ockam.aarch64-apple-darwin"
       sha256 "7d98d05f5e663746e93e81a85cf66c116f9b029538b59c388d33e8d5bf2af2cb"
 
       def install
@@ -19,7 +19,7 @@ class Ockam < Formula
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.99.0/ockam.x86_64-apple-darwin"
+      url "https://github.com/metaclips/ockam/releases/download/ockam_v0.99.0/ockam.x86_64-apple-darwin"
       sha256 "9d220d3a10d2910f27d3c1803be77f310ffe5f31c7acf85df74c2ab190c0fb8d"
 
       def install
@@ -32,7 +32,7 @@ class Ockam < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.99.0/ockam.aarch64-unknown-linux-musl"
+      url "https://github.com/metaclips/ockam/releases/download/ockam_v0.99.0/ockam.aarch64-unknown-linux-musl"
       sha256 "5f2780abb36257ffc09b3a631ce9a6c34bb5648e8e47af21e0df64fd89827f67"
 
       def install
@@ -43,7 +43,7 @@ class Ockam < Formula
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.99.0/ockam.x86_64-unknown-linux-musl"
+      url "https://github.com/metaclips/ockam/releases/download/ockam_v0.99.0/ockam.x86_64-unknown-linux-musl"
       sha256 "21c4f3894d06943b6d3c4c035a5e571af4af3cf236720fec857b9dfb64ffef0d"
 
       def install
